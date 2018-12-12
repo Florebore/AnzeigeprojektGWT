c9a86622e967b739fdd178c68ffbabb87867b1c1
Florian <Florian@FBPC>
2018-12-12 14:19:50 +0100

    Added DownloadServlet works now...!!

--- src/main/java/com/flope/servlets/FileDownloadServlet.java
+++ src/main/java/com/flope/servlets/FileDownloadServlet.java
@@ -5,10 +5,19 @@
  */
 package com.flope.servlets;
 
+import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
+import java.io.PrintWriter;
+import static java.lang.System.out;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.sql.Blob;
+import java.util.Enumeration;
 import javax.servlet.ServletException;
+import javax.servlet.ServletOutputStream;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -24,12 +33,32 @@ import javax.servlet.http.HttpServletResponse;
 public class FileDownloadServlet extends HttpServlet {
     private final int ARBITARY_SIZE = 1048;
  
+    String image = "null";
+    
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) 
       throws ServletException, IOException {
-     
-        resp.setContentType("image/jpg");
+        
+        //Http HeaderList Print
+    Enumeration headerNames = req.getHeaderNames();
+    
+    while(headerNames.hasMoreElements()) {
+         String paramName = (String)headerNames.nextElement();
+         out.print("<tr><td>" + paramName + "</td>\n");
+         String paramValue = req.getHeader(paramName);
+         out.println("<td> " + paramValue + "</td></tr>\n");
+      }
+      out.println("</table>\n</body></html>");
+      
+       
+  
+        resp.addHeader("Access-Control-Allow-Origin", "*");
+        resp.setContentType("image/jpeg");
+        resp.addHeader("Access-Control-Allow-Headers","Origin, Content-Type, Accept");      
+        resp.addHeader("Access-Control-Allow-Methods", "GET, HEAD, POST, PUT, DELETE, OPTIONS");
         resp.setHeader("Content-disposition", "attachment; filename=sample.jpg");
+        
+        
         
         String path = req.getServletContext().toString();
         System.out.println(path +"hjier");
@@ -37,15 +66,25 @@ public class FileDownloadServlet extends HttpServlet {
         try(InputStream in = req.getServletContext().getResourceAsStream("/WEB-INF/Anzeige1.jpeg");
                 
   
-          OutputStream out = resp.getOutputStream()) {
- 
+          ServletOutputStream out = resp.getOutputStream()) {
             byte[] buffer = new byte[ARBITARY_SIZE];
          
-            int numBytesRead;
-            while ((numBytesRead = in.read(buffer)) > 0) {
-                out.write(buffer, 0, numBytesRead);
+            int read=0;
+            while ((read = in.read(buffer)) != -1) {
+                
+                         
+                out.write(buffer, 0, read);
+                System.out.println(read);
+               
             }
+         out.flush();
+         out.close();
+         in.close();
         }
+        
+      
+        
+        
     }
 }
     
